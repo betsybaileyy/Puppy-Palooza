708900351f539c1cc8563a311861128a2b39cbec
betsybaileyy <betsy.bailey80@gmail.com>
2019-04-10 14:31:07 -0700

    updated search to populate by both name and breed

--- routes/pets.js
+++ routes/pets.js
@@ -6,9 +6,12 @@ module.exports = (app) => {
 
   // SEARCH PET
   app.get('/search', (req, res) => {
-  term = new RegExp(req.query.term, 'i')
+    term = new RegExp(req.query.term, 'i')
 
-    Pet.find({'name': term}).exec((err, pets) => {
+    Pet.find({$or:[
+      {'name': term},
+      {'species': term}
+    ]}).exec((err, pets) => {
       res.render('pets-index', { pets: pets });
     })
   });
